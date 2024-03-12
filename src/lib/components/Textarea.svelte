33611007d333506752d76d903eeb90cbaf42ca50
Johnny <johnny.gomes@ufjf.br>
2024-03-12 12:41:09 -0300

    feat: Edit activity

---
+++ src/lib/components/Textarea.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLTextareaAttributes {
+		blurAtStart?: boolean;
+		value?: string;
+		label: string;
+	}
+
+	export let value: string = '';
+</script>
+
+{#if $$props.label}
+	<label for={$$props.id || $$props.name} class="block text-sm font-medium mb-1">
+    {$$props.label}
+  </label>
+{/if}
+
+<textarea
+	{...$$props}
+	placeholder={$$props.placeholder || $$props.label}
+	id={$$props.id || $$props.name}
+	class="shadow appearance-none border rounded border-gray-800 dark:border-gray-100 w-full py-2 px-3
+          leading-tight text-sm focus:outline-none ring-primary-500 focus:ring-2 bg-transparent"
+	bind:value
+	on:blur
+	on:change
+  on:keypress
+></textarea>
--- src/routes/(spa)/activities/[id]/edit/+page.svelte
+++ src/routes/(spa)/activities/[id]/edit/+page.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import Button from '$lib/components/Button.svelte';
 	import TextInput from '$lib/components/TextInput.svelte';
+	import Textarea from '$lib/components/Textarea.svelte';
 	import { activities } from '$lib/stores/activities-store';
-
-	let inputName: HTMLInputElement;
 
 	$: activity = $activities.find((a) => a.id === $page.params.id);
 
-	function submit() {
-
+	function submit(e: SubmitEvent) {
+		e.preventDefault();
+		const formData = new FormData(e.target as HTMLFormElement);
+		
+		const id = activity?.id ?? '';
+		const name = String(formData.get('name'));
+		const description = String(formData.get('description'));
+		
+		activities.update(id, name, description);
+		goto(`/activities/${id}`);
 	}
 </script>
 
-<div
+<form
 	class="flex flex-col gap-2 mb-4 max-w-screen-md border rounded-lg border-gray-200 dark:border-gray-700 p-4"
+	on:submit={submit}
 >
 	<div class="w-full max-w-[360px]">
 		<TextInput
 			name="name"
 			value={activity?.name}
 			label="Activity name"
-			bind:inputElement={inputName}
-			on:keypress={evt => {
-				if (evt.key === 'Enter') {
-					submit();
-				}
-			}}
+			required
 		/>
 	</div>
-</div>
+
+	<div class="w-full max-w-[360px]">
+		<Textarea name="description" label="Description" value={activity?.description} rows={5} />
+	</div>
+
+	<div class="w-full flex justify-around">
+		<a href={`/activities/${activity?.id}`} class="w-1/3 md:w-auto">
+			<Button class="bg-secondary-600 hover:bg-secondary-500 text-white" type="button">
+				Cancel
+			</Button>
+		</a>
+		<Button type="submit" class="bg-teal-600 hover:bg-teal-500 text-white">Update</Button>
+	</div>
+</form>

d718e5f73085c67cf002483ee93304d3b9947e59
johnnysn <johnnysn@gmail.com>
2024-03-18 14:09:14 -0300

    feat: Improve hero text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <div class="flex flex-col w-full max-w-screen-xl px-2 md:px-6 mt-4">
 	<div class="flex flex-wrap items-center justify-center">
 		<div class="md:w-2/5 flex flex-col items-center">
-			<h1 class="text-3xl md:text-4xl lg:text-5xl font-bold font-title mb-4 md:mb-6">
-				Time to <span class="text-accent">spare</span> <br />
-				<span class="md:text-5xl lg:text-6xl">your time</span>
+			<h1 class="font-bold font-title mb-4 md:mb-6 text-center">
+				<span class="text-3xl md:text-4xl lg:text-5xl italic">Time to</span> <br />
+				<span class="text-6xl md:text-7xl lg:text-8xl text-accent uppercase tracking-wider"
+					>spare</span
+				> <br />
+				<span class="text-4xl md:text-5xl lg:text-6xl">your time</span>
 			</h1>
 			<p class="mb-4 px-6 text-center md:text-lg">
 				Learn how much time you spend on your daily activities, and start making great plans

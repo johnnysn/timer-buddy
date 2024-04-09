f3a9d358a9ce151a3e2445b75e148fde72e324e0
Carlos <carlosladeira@gmail.com>
2024-04-09 16:00:38 -0300

    feat: Optional view for Execution

--- src/routes/(spa)/plans/[id]/PlanTimer.svelte
+++ src/routes/(spa)/plans/[id]/PlanTimer.svelte
@@ -11,9 +11,12 @@
 
 	export let averageDuration: number;
 	export let planExecution: PlanExecution;
+
+	let linearView = true;
 </script>
 
 <div class="w-full max-w-screen-md border rounded-lg border-gray-200 dark:border-gray-700 p-4 mt-4">
+{#if linearView}
 	<div class="flex flex-row pl-2"><strong class="mr-2">Now:</strong><Clock /></div>
 	<p>
 		<span class="flex flex-row mt-1 pl-2">
@@ -33,8 +36,54 @@
 			<span>{dateFormatter.format(planExecution.target)}</span>
 		</span>
 	</p>
+{/if}
+
+{#if !linearView}
+	<div class="flex flex-row">
+		<div class="max-w-11 max-h-72 md:max-h-64">
+			<img src="/time_arrow.png" alt="Arrow time"  style="height: auto; max-height: 100%;" />
+		</div>
+		<div class="w-full">
+			<div class="flex flex-row pl-2 py-2 border border-gray-200"  ><strong class="mr-2">Now:</strong><Clock /></div>
+
+			<div class="border-t-2 border-x-2 border-gray-200 mt-2 bg-main-light dark:bg-main-dark py-2">
+				<div class="pl-20 md:pl-60"><img src="/hourglass.webp" width="25" height="25" /></div>
+			</div>
+			<div class="flex flex-row justify-between w-full">
+				<div class="flex justify-center border-r-2 border-y-2 border-gray-200 w-1/2 md:w-3/4">
+					<p class="flex justify-center w-full">
+						<span class="flex flex-col w-full my-2 mr-2 pl-2 py-2 border border-gray-200">
+							<div class="flex flex-col md:flex-row justify-items-center">
+								<div class="flex justify-center"><strong class="mr-2">Plan duration:</strong></div>
+								<div class="flex justify-center">{intervalFormatter.format(averageDuration)}</div>
+							</div>
+						</span>
+					</p>
+				</div>
+				<div class="flex justify-center border-r-2 border-gray-200 w-1/2 md:w-1/4 bg-main-light dark:bg-main-dark">
+					<span class="flex flex-col mt-1">
+						<strong class="flex justify-center">Remaining</strong>
+						<strong class="flex justify-center">time:</strong>
+						<div class="flex justify-center"><Countdown targetTime={planExecution.target} {averageDuration} /></div>
+					</span>
+				</div>
+			</div>
+			<div class="border-b-2 border-x-2 border-gray-200 bg-main-light dark:bg-main-dark mb-2 py-2">
+				<div class="pl-20 md:pl-60"><img src="/hourglass.webp" width="25" height="25" /></div>
+			</div>
+
+			<p class="border border-gray-200">
+				<span class="flex flex-row mt-1 pl-2 py-2">
+					<strong class="pr-2">Target time:</strong>
+					<span>{dateFormatter.format(planExecution.target)}</span>
+				</span>
+			</p>
+		</div>
+	</div>
+{/if}
 
 	<div class="flex justify-around items-center">
+		<Button class="primary" on:click={() => linearView = !linearView}>Toggle view</Button>
 		<Button class="secondary" on:click={() => dispatch('finish')}>Finish</Button>
 	</div>
 </div>

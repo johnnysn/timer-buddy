dcdedcd14c2f1f367b0514f094bf422f8d60368e
Carlos <carlosladeira@gmail.com>
2024-04-11 10:52:25 -0300

    fix: Dark mode for Landing and About pages

--- src/routes/(landing)/+layout.svelte
+++ src/routes/(landing)/+layout.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
+	import { darkModeStore } from '$lib/stores/dark-mode-store';
 	import Header from './Header.svelte';
+
+	$: darkModeClass = $darkModeStore ? 'dark' : '';
 </script>
 
-<div class="min-h-screen bg-white text-black flex flex-col">
-	<Header />
+<div class={darkModeClass}>
+	<div class="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300 flex flex-col">
+		<Header />
 
-	<div class="w-full flex justify-center flex-1">
-		<slot />
+		<div class="w-full flex justify-center flex-1">
+			<slot />
+		</div>
+
+		<footer class="py-6 text-center">Copyright &copy; {new Date().getFullYear()}</footer>
 	</div>
-
-	<footer class="py-6 text-center">Copyright &copy; {new Date().getFullYear()}</footer>
 </div>
--- src/routes/(landing)/Header.svelte
+++ src/routes/(landing)/Header.svelte
@@ -1,23 +1,41 @@
 <script>
-	import { Activity, CircleHelp, NotepadText, Play, PlayCircle } from 'lucide-svelte';
+	import Toggle from '$lib/components/Toggle.svelte';
+	import { darkModeStore } from '$lib/stores/dark-mode-store';
 </script>
 
-<header class="w-full flex justify-center bg-white mb-4">
+<header class="w-full flex justify-center bg-main-light dark:bg-main-dark mb-4">
 	<div class="w-full h-20 px-2 md:px-6 flex justify-between items-center max-w-screen-xl">
-		<a href="/" class="flex items-center gap-2">
-			<img src="/Logo.png" alt="Timer Buddy" width="95" height="75" class="w-auto h-[48px]" />
-			<h1 class="text-lg font-title leading-tight">
-				<span class="text-main-dark font-bold italic text-lg leading-none">Timer</span> <br />
-				<span class="font-semibold">Buddy</span>
-			</h1>
-		</a>
+		<div class="w-2/5">
+			<a href="/" class="flex items-center gap-2">
+				{#if $darkModeStore}
+					<img
+						src="/Logo-Dark.png"
+						alt="Timer Buddy"
+						width="95"
+						height="75"
+						class="w-auto h-[48px]"
+					/>
+				{:else}
+					<img src="/Logo.png" alt="Timer Buddy" width="95" height="75" class="w-auto h-[48px]" />
+				{/if}
+				<h1 class="text-lg font-title leading-tight">
+					<span class="text-main-dark dark:text-main-light font-bold italic text-lg leading-none"
+						>Timer</span
+					> <br />
+					<span class="font-semibold">Buddy</span>
+				</h1>
+			</a>
+		</div>
 
-		<div class="flex items-center">
-			<ul class="ml-3 flex items-center gap-4 md:gap-4">
-				<li></li>
+		<div class="flex justify-end items-center w-3/5">
+			<div class="flex items-center border-2 rounded-full border-gray-300 dark:border-gray-800 p-3">
+				<Toggle bind:checked={$darkModeStore} />
+			</div>
+			
+			<ul class="ml-2 md:ml-6 flex items-center gap-2 md:gap-4">
 				<li>
 					<a href="/activities" class="flex items-center gap-1 hover:underline">
-						<span class="font-medium">Start now</span>
+						<span class="flex justify-center font-medium">Start now</span>
 					</a>
 				</li>
 				<li>
@@ -25,7 +43,6 @@
 						<span class="font-medium">About</span>
 					</a>
 				</li>
-				<li></li>
 			</ul>
 		</div>
 	</div>

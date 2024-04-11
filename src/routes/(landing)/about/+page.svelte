182d090b0b418afa2e0821584c47e6e04a2cced5
Carlos <carlosladeira@gmail.com>
2024-04-11 11:46:29 -0300

    fix: About page structure

--- src/routes/(landing)/about/+page.svelte
+++ src/routes/(landing)/about/+page.svelte
@@ -1,13 +1,40 @@
-<div class="flex flex-col w-full max-w-screen-md mt-6">
-	<p class="font-light pb-4">
-		Timer Buddy is a non-commercial software which main purpose is a technical study of related
-		technologies used and for portfolio enhancement of it's creators
-	</p>
-	<p class="font-light pb-4">The project is made under MTI License and ...</p>
-	<p class="font-light pb-16">See the table below with the limits of use for Timer Buddy</p>
-	<p class="font-light pb-4"><strong>GIVE</strong> feedback for the creators of Timer Buddy</p>
-	<p class="font-light">
-		See <strong>HERE</strong> the information about the technologies used in the project and about it's
-		creators
-	</p>
+<div class="flex flex-col w-full max-w-screen-md mt-2">
+
+	<div class="flex flex-col md:flex-row mx-6 mb-4">
+		<div class="w-full md:w-1/2 text-justify font-light mb-4 md:mr-6">
+			<h1 class="font-bold">The project</h1>
+			<span>Timer Buddy is a non-commercial software which main purpose is a technical study of related
+			technologies used and for portfolio enhancement of it's creators</span>
+		</div>
+
+		<div class="w-full md:w-1/2 text-justify font-light">
+			<h1 class="font-bold">Licence</h1>
+			The project is made under MTI License and...
+		</div>
+	</div>
+
+	<div class="flex flex-col md:flex-row mx-6 mb-4">
+		<div class="w-full md:w-1/2 text-justify font-light mb-4 md:mr-6">
+			<h1 class="font-bold">Repo</h1>
+			<strong>HERE</strong> you can access the repo of the project
+		</div>
+
+		<div class="w-full md:w-1/2 text-justify font-light">
+			<h1 class="font-bold">Technology</h1>
+			See <strong>HERE</strong> the information about the technologies used in the project
+		</div>
+	</div>
+
+	<div class="flex flex-col md:flex-row mx-6 mb-4">
+		<div class="w-full md:w-1/2 text-justify font-light mb-4 md:mr-6">
+			<h1 class="font-bold">Creators</h1>
+			See <strong>HERE</strong> about the creators of Timer Buddy
+		</div>
+
+		<div class="w-full md:w-1/2 text-justify font-light">
+			<h1 class="font-bold">Feedback</h1>
+			If you want to feedback...
+		</div>
+	</div>
+
 </div>

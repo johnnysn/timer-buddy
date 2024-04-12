64ab09b958b33c06c0aa8a7138be93ee823e35c4
johnnysn <johnnysn@gmail.com>
2024-04-12 15:18:13 -0300

    fix: Add target blank to the links provided by the about page

--- src/routes/(landing)/about/+page.svelte
+++ src/routes/(landing)/about/+page.svelte
@@ -13,36 +13,42 @@
 	<div class="mb-4">
 		<h2 class="text-xl font-title font-semibold mb-1">Licence</h2>
 		<p class="font-light font-lg">
-			The project is made under MTI License and adheres to its terms and conditions, supporting a
-			collaborative development environment.
+			The project is made under <a href="https://opensource.org/license/mit" target="_blank"
+				>MIT License</a
+			> and adheres to its terms and conditions, supporting a collaborative development environment.
 		</p>
 	</div>
 
 	<div class="mb-4">
 		<h2 class="text-xl font-title font-semibold mb-1">Repo</h2>
 		<p class="font-light font-lg">
-			<a href="https://github.com/johnnysn/timer-buddy">HERE</a> you can access the repository of the
-			project.
+			<a href="https://github.com/johnnysn/timer-buddy" target="_blank">HERE</a> you can access the repository
+			of the project.
 		</p>
 	</div>
 
 	<div class="mb-4">
 		<h2 class="text-xl font-title font-semibold mb-1">Technology</h2>
 		<p class="font-light font-lg">
-			This project is built with <a href="https://kit.svelte.dev/docs/introduction">SvelteKit</a>
-			framework and powered by <a href="https://tailwindcss.com/">Tailwind CSS</a>.
+			This project is built with <a href="https://kit.svelte.dev/docs/introduction" target="_blank"
+				>SvelteKit</a
+			>
+			framework and powered by <a href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a>.
 		</p>
 	</div>
 
 	<div class="mb-4">
 		<h2 class="text-xl font-title font-semibold mb-1">Creators</h2>
 		<p class="font-light font-lg">
-			This software was created and is maintained by <a href="https://github.com/carlosladeira"
-				>Carlos Ladeira</a
+			This software was created and is maintained by <a
+				href="https://github.com/carlosladeira"
+				target="_blank"
 			>
+				Carlos Ladeira
+			</a>
 			and
-			<a href="https://github.com/johnnysn">Johnny Moreira</a>. Feel free to provide us with
-			feedback about the project anytime.
+			<a href="https://github.com/johnnysn" target="_blank"> Johnny Moreira</a>. Feel free to
+			provide us with feedback about the project anytime.
 		</p>
 	</div>
 </div>

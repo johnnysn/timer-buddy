8345d0261e2165ad0428b9d7c0e9a30075f2bb25
johnnysn <johnnysn@gmail.com>
2024-04-12 10:11:56 -0300

    feat: Complete project information on the about page.

--- src/routes/(landing)/about/+page.svelte
+++ src/routes/(landing)/about/+page.svelte
@@ -1,40 +1,50 @@
 <div class="flex flex-col w-full max-w-screen-md mt-2">
+	<h1 class="font-title text-2xl font-bold mb-4">About Timer Buddy</h1>
 
-	<div class="flex flex-col md:flex-row mx-6 mb-4">
-		<div class="w-full md:w-1/2 text-justify font-light mb-4 md:mr-6">
-			<h1 class="font-bold">The project</h1>
-			<span>Timer Buddy is a non-commercial software which main purpose is a technical study of related
-			technologies used and for portfolio enhancement of it's creators</span>
-		</div>
-
-		<div class="w-full md:w-1/2 text-justify font-light">
-			<h1 class="font-bold">Licence</h1>
-			The project is made under MTI License and...
-		</div>
+	<div class="mb-4">
+		<h2 class="text-xl font-title font-semibold mb-1">The project</h2>
+		<p class="font-light font-lg">
+			Timer Buddy is a non-commercial software primarily designed for in-depth exploration of
+			associated technologies, serving as both a technical study tool and a portfolio enhancement
+			project for its creators.
+		</p>
 	</div>
 
-	<div class="flex flex-col md:flex-row mx-6 mb-4">
-		<div class="w-full md:w-1/2 text-justify font-light mb-4 md:mr-6">
-			<h1 class="font-bold">Repo</h1>
-			<strong>HERE</strong> you can access the repo of the project
-		</div>
-
-		<div class="w-full md:w-1/2 text-justify font-light">
-			<h1 class="font-bold">Technology</h1>
-			See <strong>HERE</strong> the information about the technologies used in the project
-		</div>
+	<div class="mb-4">
+		<h2 class="text-xl font-title font-semibold mb-1">Licence</h2>
+		<p class="font-light font-lg">
+			The project is made under MTI License and adheres to its terms and conditions, supporting a
+			collaborative development environment.
+		</p>
 	</div>
 
-	<div class="flex flex-col md:flex-row mx-6 mb-4">
-		<div class="w-full md:w-1/2 text-justify font-light mb-4 md:mr-6">
-			<h1 class="font-bold">Creators</h1>
-			See <strong>HERE</strong> about the creators of Timer Buddy
-		</div>
-
-		<div class="w-full md:w-1/2 text-justify font-light">
-			<h1 class="font-bold">Feedback</h1>
-			If you want to feedback...
-		</div>
+	<div class="mb-4">
+		<h2 class="text-xl font-title font-semibold mb-1">Repo</h2>
+		<p class="font-light font-lg">
+			<a href="https://github.com/johnnysn/timer-buddy">HERE</a> you can access the repository of the
+			project.
+		</p>
 	</div>
 
+	<div class="mb-4">
+		<h2 class="text-xl font-title font-semibold mb-1">Technology</h2>
+		<p class="font-light font-lg">
+			This project is built with <a href="https://kit.svelte.dev/docs/introduction">SvelteKit</a>
+			framework and powered by <a href="https://tailwindcss.com/">Tailwind CSS</a>.
+		</p>
+	</div>
+
+	<div class="mb-4">
+		<h2 class="text-xl font-title font-semibold mb-1">Creators</h2>
+		<p class="font-light font-lg">
+			This software was created and is maintained by <a href="https://github.com/carlosladeira">Carlos Ladeira</a> and 
+			<a href="https://github.com/johnnysn">Johnny Moreira</a>. Feel free to provide us with feedback about the project anytime.
+		</p>
+	</div>
 </div>
+
+<style>
+	a {
+		@apply font-medium cursor-pointer;
+	}
+</style>

497b20368dc884d19cdfe7475a92301f2e48f7ef
Carlos <carlosladeira@gmail.com>
2024-04-19 10:49:55 -0300

    feat: Cards and new layout for landing page

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -1,24 +1,53 @@
 <div class="flex flex-col w-full max-w-screen-xl px-2 md:px-6 mt-4">
 	<div class="flex flex-wrap items-center justify-center">
-		<div class="md:w-2/5 flex flex-col items-center">
-			<h1 class="font-bold font-title mb-4 md:mb-6 text-center">
-				<span class="text-3xl md:text-4xl lg:text-5xl italic">Time to</span> <br />
-				<span class="text-6xl md:text-7xl lg:text-8xl text-accent uppercase tracking-wider"
-					>spare</span
-				> <br />
-				<span class="text-4xl md:text-5xl lg:text-6xl">your time</span>
-			</h1>
-			<p class="mb-4 px-4 text-center md:text-lg">
-				Learn how much time you spend on your daily activities, and start making great plans
-			</p>
-			<a
-				href="/activities"
-				class="rounded-full px-6 py-3 text-white bg-main-dark hover:bg-main transition-colors duration-300 mb-4 md:mb-6"
-			>
-				Start now
-			</a>
+
+		<div class="flex flex-col md:flex-row md:justify-center">
+			<div class="md:w-1/2 flex flex-col items-center">
+				<div class="flex flex-row justify-around">
+					<h1 class="font-bold font-title mt-2 md:mb-6 text-center">
+						<span class="text-2xl md:text-4xl lg:text-5xl italic">Time to</span> <br />
+						<span class="text-4xl md:text-7xl lg:text-8xl text-accent uppercase tracking-wider"
+							>spare</span
+						> <br />
+						<span class="text-2xl md:text-5xl lg:text-6xl">your time</span>
+					</h1>
+					<img src="/Hero-No-BG.png" alt="Timer Buddy" width="40%" height="40%" class="block md:hidden"/>
+				</div>
+				<p class="mb-4 px-4 text-center md:text-lg">
+					Learn how much time you spend on your daily activities, and start making great plans
+				</p>
+				<a
+					href="/activities"
+					class="rounded-full px-6 py-3 text-white bg-main-dark hover:bg-main transition-colors duration-300 mb-4 md:mb-6"
+				>
+					Start now
+				</a>
+			</div>
+
+			<div class="hidden md:block md:w-1/2">
+				<div class="flex flex-row justify-center"><img src="/Hero-No-BG.png" alt="Timer Buddy" width="60%" height="60%"/></div>
+			</div>
 		</div>
 
-		<img src="/Hero-No-BG.png" alt="Timer Buddy" width="980" height="924" class="w-full md:w-3/5" />
+		<section class="cards">
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+				<div class="card bg-white rounded-lg px-4 py-2 md:py-4 shadow-md">
+					<h3 class="text-lg font-bold mb-2">Understand your time better</h3>
+					<p class="text-gray-700">Discover how much time you dedicate to each activity and gain valuable insights to optimize your daily life</p>
+				</div>
+
+				<div class="card bg-white rounded-lg px-4 py-2 md:py-4 shadow-md">
+					<h3 class="text-lg font-bold mb-2">Plan and execute plans</h3>
+					<p class="text-gray-700">Create personalized plans, group activities, view total time and project them into your routine. Monitor progress and maximize your free time</p>
+				</div>
+
+				<div class="card bg-white rounded-lg px-4 py-2 md:py-4 shadow-md">
+					<h3 class="text-lg font-bold mb-2">Local Storage and Backup</h3>
+					<p class="text-gray-700">Use local storage, no login required. Backup and restore your information to transfer your data between devices</p>
+				</div>
+			</div>
+		</section>
+
 	</div>
 </div>

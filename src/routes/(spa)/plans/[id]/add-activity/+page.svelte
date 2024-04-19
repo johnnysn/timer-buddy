d3d6e7063c184b90b55246b5b7c808373b6297d1
Carlos <carlosladeira@gmail.com>
2024-04-19 11:32:08 -0300

    feat: Text search to activities of a plan

--- src/routes/(spa)/plans/[id]/add-activity/+page.svelte
+++ src/routes/(spa)/plans/[id]/add-activity/+page.svelte
@@ -5,12 +5,37 @@
 	import { activities } from '$lib/stores/activities-store.js';
 	import { plans } from '$lib/stores/plans-store.js';
 	import intervalFormatter from '$lib/utils/interval-formatter.js';
+	import { Search } from 'lucide-svelte';
+import type { Activity } from '$lib/types/activity';
+	import TextInput from '$lib/components/TextInput.svelte';
 
 	$: returnUrl = `/plans/${$page.params.id}`;
+	let searchTimer = 0;
+	let searchInputValue = '';
+	let searchKey = '';
+	let filteredActivities: Activity[] = [];
 
 	function addActivity(activityId: string) {
 		plans.addActivity($page.params.id, activityId);
 		goto(returnUrl);
+	}
+
+	function searchDebounce(e: Event) {
+		const { value } = e.target as HTMLInputElement;
+		clearTimeout(searchTimer);
+		searchTimer = setTimeout(() => {
+			searchKey = value;
+		}, 200);
+	}
+	$: {
+		filteredActivities = $activities.filter((a) => {
+			const key = searchKey.trim().toLowerCase();
+			if (key !== '') {
+				return a.name.trim().toLowerCase().includes(key);
+			} else {
+				return true;
+			}
+		});
 	}
 </script>
 
@@ -24,8 +49,26 @@
 	</a>
 </div>
 
+<div class="w-full flex justify-center gap-3 items-center my-4">
+	<TextInput
+		name="search"
+		placeholder="Search activity"
+		label=""
+		maxlength={40}
+		on:input={searchDebounce}
+		bind:value={searchInputValue}
+	/>
+
+	<button
+		class="rounded p-1 bg-main text-white"
+		on:click={() => (searchKey = searchInputValue)}
+	>
+		<Search class="size-6" />
+	</button>
+</div>
+
 <ul class="flex flex-col gap-2">
-	{#each $activities as activity (activity.id)}
+	{#each filteredActivities as activity (activity.id)}
 		<li>
 			<button
 				class="item-container px-2 w-full max-w-screen-md flex flex-col"
